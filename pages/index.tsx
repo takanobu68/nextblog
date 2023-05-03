8d5d2fb5a3125bd9e5f4fab18b1ec90e70393140
Takanobu <61861946+takanobu68@users.noreply.github.com>
2023-05-03 13:27:16 +0900

    Cardコンポーネントの作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
+import Card from '@/components/Card';
 import Layout from '@/components/Layout';
 import { siteConfig } from '@/site.config';
+import { sampleCards } from '@/utils/sample';
 
 export default function Home() {
   return (
@@ -8,6 +10,9 @@ export default function Home() {
         <h1 className='text-5xl mb-8'>{siteConfig.title}</h1>
         <div className='grid md:gap-6 mt-10 md:grid-cols-2 w-full my-12'>
           {/* Card */}
+          {sampleCards.map((page) => (
+            <Card page={page} />
+          ))}
         </div>
       </div>
     </Layout>
