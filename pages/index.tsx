320fc5bba640fba5a6d8191f46e108990cd86fe7
Takanobu <61861946+takanobu68@users.noreply.github.com>
2023-05-03 16:14:33 +0900

    indexページに型情報を設定

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,26 @@
 import Card from '@/components/Card';
 import Layout from '@/components/Layout';
 import { siteConfig } from '@/site.config';
+import { IndexProps } from '@/types/types';
 import { fetchPage } from '@/utils/notion';
 import { sampleCards } from '@/utils/sample';
-import { GetStaticProps } from 'next';
+import { GetStaticProps, NextPage } from 'next';
 
-export default function Home({ pages }) {
+const Home: NextPage<IndexProps> = ({ pages }) => {
   return (
     <Layout>
       <div className='pt-12'>
         <h1 className='text-5xl mb-8'>{siteConfig.title}</h1>
         <div className='grid md:gap-6 mt-10 md:grid-cols-2 w-full my-12'>
           {/* Card */}
-          {sampleCards.map((page) => (
+          {pages.map((page) => (
             <Card page={page} />
           ))}
         </div>
       </div>
     </Layout>
   );
-}
+};
 
 export const getStaticProps: GetStaticProps = async () => {
   const { results } = await fetchPage();
@@ -31,3 +32,5 @@ export const getStaticProps: GetStaticProps = async () => {
     revalidate: 10,
   };
 };
+
+export default Home;
