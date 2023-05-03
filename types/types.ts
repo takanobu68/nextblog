1e763acdd2788205f54c916513c99e7ae3dd91c5
Takanobu <61861946+takanobu68@users.noreply.github.com>
2023-05-03 18:16:06 +0900

    型情報の追加

--- types/types.ts
+++ types/types.ts
@@ -16,7 +16,11 @@ export type PageProps = {
 };
 
 export type CardProps = { page: PageType };
-export type ArticleProps = CardProps;
+export type ArticleProps = {
+  page: PageType;
+  blocks: BlockType[];
+};
+
 export type ArticleMetaProps = CardProps;
 
 export type IndexProps = { pages: PageType[] };
@@ -59,4 +63,11 @@ export type PageType = {
   cover: FileType | null;
   // properties: Record<string, any>;
   properties: PropertyType;
+};
+
+export type BlockType = {
+  type: string;
+  heading_1: { rich_text: RichTextType[] };
+  heading_2: { rich_text: RichTextType[] };
+  paragraph: { rich_text: RichTextType[] };
 };
