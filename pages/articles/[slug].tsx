1b359dd88402d840329f55f2597a26874d35a162
Takanobu <61861946+takanobu68@users.noreply.github.com>
2023-05-03 18:15:54 +0900

    関数名の変更

--- pages/articles/[slug].tsx
+++ pages/articles/[slug].tsx
@@ -1,12 +1,15 @@
 import ArticleMeta from '@/components/ArticleMeta';
 import Layout from '@/components/Layout';
 import { ArticleProps, Params } from '@/types/types';
-import { fetchBlocksByPageId, fetchPage } from '@/utils/notion';
+import { fetchBlocksByPageId, fetchPages } from '@/utils/notion';
 import { getText } from '@/utils/property';
 import { sampleCards } from '@/utils/sample';
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 
-const Article: NextPage<ArticleProps> = ({ page }) => {
+const Article: NextPage<ArticleProps> = ({ page, blocks }) => {
+  console.log('page: ', page);
+  console.log('blocks: ', blocks);
+  return <></>;
   return (
     <Layout>
       <article className='w-full'>
@@ -25,7 +28,7 @@ const Article: NextPage<ArticleProps> = ({ page }) => {
 export default Article;
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const { results } = await fetchPage({});
+  const { results } = await fetchPages({});
   const paths = results.map((page: any) => {
     return {
       params: {
@@ -41,7 +44,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
   const { slug } = ctx.params as Params;
-  const { results } = await fetchPage({ slug });
+  const { results } = await fetchPages({ slug });
   const page = results[0];
   const pageId = page.id;
 
