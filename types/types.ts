70cb08cdc7094cf2b97c6415c96cfb0f29ba0104
Takanobu <61861946+takanobu68@users.noreply.github.com>
2023-05-03 16:08:29 +0900

    型定義情報の追加

--- types/types.ts
+++ types/types.ts
@@ -26,3 +26,38 @@ export type ArticleMetaProps = CardProps;
 export type Params = ParsedUrlQuery & {
   slug: string;
 };
+
+export type FileType = {
+  file?: { url: string };
+  external?: { url: string };
+};
+
+export type AnnotationType = {
+  bold: boolean;
+  italic: boolean;
+  strikethrough: boolean;
+  underline: boolean;
+  code: boolean;
+  color: string;
+};
+
+export type RichTextType = {
+  plain_text: string;
+  href: string | null;
+  annotation: AnnotationType;
+};
+
+export type PropertyType = {
+  name: { title: RichTextType[] };
+  author: { rich_text: RichTextType[] };
+  slug: { rich_text: RichTextType[] };
+  published: { date: { start: string } };
+  isPublic: { checkbox: boolean };
+  tags: { multi_select: [{ name: string }] };
+};
+
+export type PageType = {
+  id: string;
+  cover: FileType | null;
+  properties: PropertyType;
+};
